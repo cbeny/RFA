b91c562c86bb4aa23a3880b371a45fc08ce70d94
Cedric Beny <cbeny@qit.hanyang.ac.kr>
2019-06-05 18:06:28 +0900

    Added comments

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
 
-# Install TensorFlow
+# Install TensorFlow version 2
 import os
 os.system('pip3 install -q tensorflow==2.0.0-alpha0')
 
@@ -17,19 +16,26 @@ y_train, y_test = tf.one_hot(y_train, 10), tf.one_hot(y_test, 10)
 
 print("Using an unregularized 2-hidden layer perceptron")
 
+# Number of features = number of categories for supervised learning
+num_feat = 10  
+
+# This network produces the features on images (variable X)
+# The One-hot encoding of the labels (variable Y) already are the optimal target features
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(1024, activation='relu'),
   tf.keras.layers.Dense(1024, activation='relu'),
-  tf.keras.layers.Dense(10) 
+  tf.keras.layers.Dense(num_feat) 
 ])
 
-
+# cross-entropy loss for comparison
 def CE_Loss(x,y):
     return tf.keras.backend.categorical_crossentropy(x, y, from_logits=True)
 
-D = tf.constant(1e-8 * np.identity(10), tf.float32)
 
+D = tf.constant(1e-8 * np.identity(num_feat), tf.float32)
+
+# computes the covariances between batches of features F of X and G of Y
 def cov(F, G):
 	n = tf.cast(tf.shape(G)[0], tf.float32)  # batch size
 	K = la.matmul(la.transpose(F)/n, F)
@@ -37,16 +43,18 @@ def cov(F, G):
 	A = la.matmul(la.transpose(F)/n, G)
 	return K, L, A
 
+# relevance of features given their covariances
 def relevance(K, L, A):
 	return la.trace(la.inv(K + D) @ A @ la.inv(L + D) @ la.transpose(A))
 
+# produces a matrix which maps a vector of features on X to the inferred (expected) value of Y
 def inferY(K, L, A, G, Y):
 	n = tf.cast(tf.shape(G)[0], tf.float32)
 	return la.transpose(Y)/n @ G @ la.inv(L + D) @ la.transpose(A) @ la.inv(K + D)
 
 def RFA_Loss(F, G):
 	K, L, A = cov(F, G)
-	return 10 - relevance(K, L, A)
+	return num_feat - relevance(K, L, A)
 
 
 use_RFA = True
@@ -60,7 +68,7 @@ if use_RFA:
 		model.fit(x_train, y_train, epochs=1, batch_size=200)
 
 		F = model.predict(x_train, batch_size=200)
-		G = y_train
+		G = y_train   # (the one hot encoding already yields the optimal features)
 		K, L, A = cov(F, G)
 		I = inferY(K, L, A, G, y_train)
 
@@ -70,7 +78,7 @@ if use_RFA:
 		print("test accuracy = %.2f%%" % (acc*100))
 
 else:
-	print("and the crossentropy loss function")
+	print("and the cross-entropy loss function")
 
 	model.compile(optimizer='adam', loss = CE_Loss)
 
