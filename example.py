a4bddb1907c48688d39554b3a96344c5ddaaa268
Cedric Beny <cedric.beny@gmail.com>
2019-06-06 12:17:01 +0900

    cosmetics

--- example.py
+++ example.py
@@ -1,25 +1,22 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-# Tested on TensorFlow version 2
-#import os
-#os.system('pip3 install -q tensorflow==2.0.0-alpha0')
+# Tested on TensorFlow 1.13.1 and 2.0.0-alpha0
 
-# batch size
-bs = 200
+bs = 200  # batch size
+num_epochs = 15
 
 # for comparisons
 use_RFA = True
 use_cnn = True
 
 # Number of features = number of categories for supervised learning
-num_feat = 10  
-
+num_feat = 10
 
 import numpy as np
 import tensorflow as tf
 from tensorflow.linalg import transpose, inv, trace
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout
 
 # prepare the data
 mnist = tf.keras.datasets.mnist
@@ -31,9 +28,9 @@ y_test = tf.one_hot(y_test, 10)
 
 
 # This network produces the features on images (variable X)
-# The One-hot encoding of the labels (variable Y) already are the optimal target features
+# The one-hot encoding of the labels (variable Y) already are the optimal target features
 if use_cnn:
-	print("Using an unregularized CNN")
+	print("Using a convolutional neural net")
 
 	model = Sequential([
 		Conv2D(32, kernel_size=(3, 3), strides=(1,1), input_shape=(28, 28, 1), activation='relu'),
@@ -45,41 +42,42 @@ if use_cnn:
 	  	Dense(num_feat) 
 	])
 else:
-	print("Using an unregularized 2-hidden layer perceptron")
+	print("Using a 3-layer perceptron")
 
 	model = Sequential([
 	  Flatten(input_shape=(28, 28, 1)),
 	  Dense(1024, activation='relu'),
-	  Dense(1024, activation='relu'),
+	  Dense(1024, activation='relu'), 
 	  Dense(num_feat) 
 	])
 
 
 D = tf.constant(1e-8 * np.identity(num_feat), tf.float32)
 
-def getbs(X):
+def get_batch_size(X):
 	return tf.cast(tf.shape(X)[0], tf.float32)
 
 # computes the covariances between batches of features F of X and G of Y
 def cov(F, G):
-	n = getbs(G)  # batch size
+	n = get_batch_size(G) 
 	K = transpose(F)/n @ F
 	L = transpose(G)/n @ G
 	A = transpose(F)/n @ G
 	return K, L, A
 
 # relevance of features given their covariances
-def relevance(K, L, A):
+def relevance(ker):
+	K, L, A = ker
 	return trace(inv(K + D) @ A @ inv(L + D) @ transpose(A))
 
 # produces a matrix which maps a vector of features on X to the inferred (expected) value of Y
-def inferY(K, L, A, G, Y):
-	n = getbs(G)  # batch size
+def inferY(ker, G, Y):
+	n = get_batch_size(G)  
+	K, L, A = ker
 	return transpose(Y)/n @ G @ inv(L + D) @ transpose(A) @ inv(K + D)
 
 def RFA_Loss(F, G):
-	K, L, A = cov(F, G)
-	return num_feat - relevance(K, L, A)
+	return num_feat - relevance(cov(F, G))
 
 # cross-entropy loss for comparison
 def CE_Loss(x,y):
@@ -90,8 +88,8 @@ if use_RFA:
 
 	model.compile(optimizer='adam', loss = RFA_Loss)
 
-	for epoch in range(100):
-		model.fit(x_train, y_train, epochs=1, batch_size=bs)
+	for epoch in range(num_epochs):
+		model.fit(x_train, y_train, epochs=1, batch_size=bs) 
 
 		# computes the features on the training images
 		F = model.predict(x_train, batch_size=bs)
@@ -100,26 +98,27 @@ if use_RFA:
 		G = y_train   
 
 		# convariances of F ang G
-		K, L, A = cov(F, G)
+		ker = cov(F, G)
 
-		# inferrence matrix
-		I = inferY(K, L, A, G, y_train)
+		# matrix mapping the output of the model to a prediction
+		I = inferY(ker, G, y_train)
 
-		# actual predictions on test data
+		# label predictions on test data
 		y_pred = model.predict(x_test, batch_size=bs) @ transpose(I)
 
-		acc = tf.reduce_mean(tf.keras.metrics.categorical_accuracy(y_pred, y_test)).numpy()
-		print("test accuracy = %.2f%%" % (acc*100))
+		inacc = 1-tf.reduce_mean(tf.keras.metrics.categorical_accuracy(y_pred, y_test)).numpy()
+		print("Epoch %d: test errors: %.2f%%" % (epoch, inacc*100))
 
 else:
 	print("and the cross-entropy loss function")
 
 	model.compile(optimizer='adam', loss = CE_Loss)
 
-	for epoch in range(10):
+	for epoch in range(num_epochs):
 		model.fit(x_train, y_train, epochs=1, batch_size=bs)
+
 		y_pred = model.predict(x_test, batch_size=bs) 
 
-		acc = tf.reduce_mean(tf.keras.metrics.categorical_accuracy(y_pred, y_test)).numpy()
-		print("test accuracy = %.2f%%" % (acc*100))
+		inacc = 1-tf.reduce_mean(tf.keras.metrics.categorical_accuracy(y_pred, y_test)).numpy()
+		print("Epoch %d: test errors: %.2f%%" % (epoch, inacc*100))
 
