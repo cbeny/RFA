fcc8d57f2d30d6b0f97f8d8591cd412578419ad3
Cedric Beny <cedric.beny@gmail.com>
2019-09-07 13:23:51 +0900

    compatibility with tensorflow 1

--- example.py
+++ example.py
@@ -1,4 +1,3 @@
-# Tested on TensorFlow 2.0.0-alpha0
 
 bs = 200  # batch size
 num_epochs = 20
@@ -12,39 +11,40 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.linalg import transpose, inv, trace
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Conv2D, Dense, Flatten, Input, concatenate
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Input, concatenate, Lambda
 
 # prepare the data
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(60000,28,28,1)/255.0
 x_test = x_test.reshape(10000,28,28,1)/255.0
-y_train = tf.one_hot(y_train, 10)
-y_test = tf.one_hot(y_test, 10)
+def one_hot(data, nclass): return np.eye(nclass, dtype=np.float32)[data]
+y_train = one_hot(y_train, 10)
+y_test = one_hot(y_test, 10)
 
 # We need one network producing features for the first data type 
 # (the images in this example)
 in1 = Input(shape=(28,28,1))
 if use_cnn:
-	print("\nUsing a convolutional neural net")
-	x = Conv2D(32, kernel_size=(3, 3), strides=(1,1), activation='relu')(in1)
-	x = Conv2D(64, kernel_size=(4, 4), strides=(2,2), activation='relu')(x)
-	x = Conv2D(64, kernel_size=(3, 3), strides=(1,1), activation='relu')(x)
-	x = Conv2D(128, kernel_size=(4, 4), strides=(2,2), activation='relu')(x)
-	x = Flatten()(x)
-	x = Dense(2048, activation='relu')(x)
+    print("\nUsing a convolutional neural net")
+    x = Conv2D(32, kernel_size=(3, 3), strides=(1,1), activation='relu')(in1)
+    x = Conv2D(64, kernel_size=(4, 4), strides=(2,2), activation='relu')(x)
+    x = Conv2D(64, kernel_size=(3, 3), strides=(1,1), activation='relu')(x)
+    x = Conv2D(128, kernel_size=(4, 4), strides=(2,2), activation='relu')(x)
+    x = Flatten()(x)
+    x = Dense(2048, activation='relu')(x)
 else:
-	print("\nUsing a 3-layer perceptron")
-	x = Flatten()(in1)
-	x = Dense(1024, activation='relu')(x)
-	x = Dense(1024, activation='relu')(x)
+    print("\nUsing a 3-layer perceptron")
+    x = Flatten()(in1)
+    x = Dense(1024, activation='relu')(x)
+    x = Dense(1024, activation='relu')(x)
 out1 = Dense(num_feat)(x)
 
 # and another network producing features for the second data type
 # In this example these are the labels, but the one-hot encoding already 
-# represents the optimal features, so we just pass them through
+# represents the optimal features, so we just pass it through
 in2 = Input(shape=(10,))
-out2 = in2
+out2 = Lambda(lambda x: x)(in2)
 
 # We will use each models separately for predictions...
 feat1 = Model(inputs=in1, outputs=out1)
@@ -57,36 +57,36 @@ model = Model(inputs=[in1, in2], outputs=concatenate([out1, out2], axis=1))
 D = tf.constant(1e-8 * np.identity(num_feat), tf.float32)
 
 def get_batch_size(X):
-	return tf.cast(tf.shape(X)[0], tf.float32)
+    return tf.cast(tf.shape(X)[0], tf.float32)
 
 # computes the covariances between batches of features F of X and G of Y
 def cov(F, G):
-	n = get_batch_size(G) 
-	K = transpose(F)/n @ F
-	L = transpose(G)/n @ G
-	A = transpose(F)/n @ G
-	return K, L, A
+    n = get_batch_size(G) 
+    K = transpose(F)/n @ F
+    L = transpose(G)/n @ G
+    A = transpose(F)/n @ G
+    return K, L, A
 
 # relevance of features given their covariances
 def relevance(ker):
-	K, L, A = ker
-	return trace(inv(K + D) @ A @ inv(L + D) @ transpose(A))
+    K, L, A = ker
+    return trace(inv(K + D) @ A @ inv(L + D) @ transpose(A))
 
 # produces a matrix which maps a vector of features on X to the inferred (expected) value of Y
 def inferY(ker, G, Y):
-	n = get_batch_size(G)  
-	K, L, A = ker
-	return transpose(Y)/n @ G @ inv(L + D) @ transpose(A) @ inv(K + D)
+    n = get_batch_size(G)  
+    K, L, A = ker
+    return transpose(Y)/n @ G @ inv(L + D) @ transpose(A) @ inv(K + D)
 
 # produces a matrix inferring X from Y
 def inferX(ker, F, X):
-	n = get_batch_size(F)
-	K, L, A = ker
-	return transpose(X)/n @ F @ inv(K + D) @ A @ inv(L + D)
+    n = get_batch_size(F)
+    K, L, A = ker
+    return transpose(X)/n @ F @ inv(K + D) @ A @ inv(L + D)
 
 def RFA_Loss(dummy, features):
-	F, G = tf.split(features, 2, axis=1)
-	return num_feat - relevance(cov(F, G)) 
+    F, G = tf.split(features, 2, axis=1)
+    return num_feat - relevance(cov(F, G)) 
 
 
 model.compile(optimizer='adam', loss=RFA_Loss)
@@ -94,26 +94,32 @@ model.compile(optimizer='adam', loss=RFA_Loss)
 # keras really wants us to have a target, although we don't.
 dummy = np.zeros(60000)
 
-for epoch in range(num_epochs):
-	model.fit([x_train, y_train], dummy, epochs=1, batch_size=bs, shuffle=True) 
+for epoch in range(1,num_epochs):
+    model.fit([x_train, y_train], dummy, epochs=1, batch_size=bs, shuffle=True, verbose=2) 
 
-	print("testing...", end='\r')
+    # test the model
+    
+    sess = tf.Session()  # because we're not using tf 2.0
 
-	# compute the features on the training images
-	F = feat1.predict(x_train, batch_size=bs)
-	G = feat2.predict(y_train, batch_size=bs)
+    # compute the features on the training images
+    F = feat1.predict(x_train, batch_size=bs, verbose=0)
+    G = feat2.predict(y_train, batch_size=bs, verbose=0)
 
-	# produce a matrix mapping the output of the model to a prediction 
-	# (average over the posterior)
-	P = inferY(cov(F, G), G, y_train)
+    # produce a matrix mapping the output of the model to a prediction 
+    # (average over the posterior)
+    P = inferY(cov(F, G), G, y_train)
 
-	# label predictions on test data
-	tF = feat1.predict(x_test, batch_size=bs)
-	y_pred = tF @ transpose(P)
+    # label predictions on test data
+    tF = feat1.predict(x_test, batch_size=bs, verbose=0)
+    y_pred = sess.run(tf.math.round(tF @ transpose(P)))
 
-	# compute the test loss for good measure
-	tG = feat2.predict(y_test, batch_size=bs)
-	test_loss = num_feat - relevance(cov(tF, tG))
+    # compute the test loss for good measure
+    tG = feat2.predict(y_test, batch_size=bs, verbose=0)
+    test_loss = sess.run(num_feat - relevance(cov(tF, tG)))
 
-	inacc = 1-tf.reduce_mean(tf.keras.metrics.categorical_accuracy(y_pred, y_test)).numpy()
-	print("Epoch %d: test loss = %.3f  test errors = %.2f%%" % (epoch, test_loss, inacc*100))
+    sess.close()
+
+    inacc = np.mean(np.max(y_pred, axis=-1) != np.max(y_test, axis=-1))
+    print("Epoch %d: test loss = %.3f  test errors = %.2f%%" % (epoch, test_loss, inacc*100))
+    
+print("Done.")
